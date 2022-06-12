13455b43528823342e599d9641ab309d9f1446b8
igor.kudlo <igor.kudlo@spiralscout.com>
2022-06-12 17:05:45 +0300

    02 create components, add static files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+
+import './scss/app.scss';
+import PizzaBlock from './components/PizzaBlock';
+
 function App() {
   return (
-    <h1>
-      Pizza store app
-    </h1>
+    <div className='wrapper'>
+      <Header />
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+            <PizzaBlock title="Мексиканская" price="500" />
+            <PizzaBlock title="Деревенская" price="350" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
